4c1075d4f212c62be7c31e34a9bf78cfa8416442
endrit-b <endriti.92@gmail.com>
2015-05-31 00:46:51 +0200

    scrapind table data for every link in session list

--- run.py
+++ run.py
@@ -18,6 +18,28 @@ def scraper():
 
     for link in br.links(text_regex="Сведения об участии депутатов в заседаниях"):
         link_url = "http://www.kenesh.kg" + link.url
-        print link_url
+        # Open absentees link
+        respose = br.open(link_url)
+        # Read content of the link and load it in endrit_soup
+        html_content = respose.read()
+        endrit_soup = BeautifulSoup(html_content)
+        table  = endrit_soup.find('table', style=lambda x: x and x.startswith('width:102.84'))
+        table_soup = table
+        table_rows = table_soup.findAll('tr')
+
+        for row in table_rows:
+            if row != 0:
+                for cell in row.findAll('td'):
+                    if cell.findAll('div'):
+                        cell_soup = cell.findAll('div')
+                        print cell_soup[0].text
+                    else:
+                        cell_soup = cell.find('ol')
+                        for attr, val in cell_soup.attrs:
+                            if attr == "start":
+                                print val
+                        #print cell_soup[0].text
+                print "----------------------"
+        break
 
 scraper()
